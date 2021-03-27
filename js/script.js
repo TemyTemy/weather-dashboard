5c2cdf7c326eba7ae868f2cf62099ecdb8c2d98a
Temy <tayolanre2000@yahoo.com>
2021-03-27 22:38:31 +0000

    UPDATE: Save weather summary to local storage

--- js/script.js
+++ js/script.js
@@ -18,3 +18,22 @@ var WEATHER_DETAILS = {
     previousSearches: []
   }
   
+  // Empty weather details
+var EMPTY_WEATHER_DETAILS = {
+
+}
+
+WEATHER_DETAILS_STORAGE_KEY = "weather";
+OPEN_WEATHER_API_KEY = "cfaac070928fb11f97b715a91a712cce";
+OPEN_WEATHER_URL =
+  "http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid=" + OPEN_WEATHER_API_KEY;
+
+OPEN_WEATHER_FORECAST_URL =
+  "http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=imperial&appid=" + OPEN_WEATHER_API_KEY;
+
+  //  Save weather summary to local storage
+function saveWeatherSummary() {
+    localStorage.setItem(WEATHER_DETAILS_STORAGE_KEY, JSON.stringify(WEATHER_DETAILS));
+   }
+
+   
