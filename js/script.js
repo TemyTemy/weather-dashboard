f19613fc81284a53e78681979a573d8292e5ddb8
Temy <tayolanre2000@yahoo.com>
2021-03-27 22:35:50 +0000

    UPDATE: Added weather details object

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Weather details object structure
+var WEATHER_DETAILS = {
+    city: '',
+    date: '',
+    temperature: '',
+    humidity: '',
+    windSpeed: '',
+    uvIndex: '',
+    flag: '',
+    forecasts: [
+      {
+        date: '',
+        temperature: '',
+        humidity: '',
+        flag: ''
+      }
+    ],
+    previousSearches: []
+  }
+  
